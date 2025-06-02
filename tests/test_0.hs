ff74999b884144f467c89c22d1c58a66cc788f96
thales3006 <thales3006@gmail.com>
2025-06-01 23:40:53 -0300

    fix: test function wrong name

--- tests/test_0.hs
+++ tests/test_0.hs
@@ -1,8 +1,8 @@
 fac1:: Int -> Int
 fac1 0 = 1
-fac1 n = n * fac (n-1)
+fac1 n = n * fac1 (n-1)
 
 fac2:: Int -> Int
 fac2 n 
     | n==0 = 1
-    | otherwise = n * fac   (n-1)
+    | otherwise = n * fac2   (n-1)
