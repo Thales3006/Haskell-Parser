418b42d098dc00199fe8ce6698ff634ccb562b83
thales3006 <thales3006@gmail.com>
2025-06-10 00:37:28 -0300

    refactor: improve code structure and formatting in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,49 @@
+mod ast;
+mod ast_build;
+
 use std::fs;
 
-use pest::{iterators::Pairs, Parser};
+use crate::{
+    ast::{HaskellParser, Rule},
+    ast_build::build_tree,
+};
+use pest::{Parser, iterators::Pairs};
 
-#[derive(pest_derive::Parser)]
-#[grammar = "haskell.pest"]
-pub struct HaskellParser;
+fn main() {
+    let file = fs::read_to_string("tests/test_0.hs").expect("Error while reading the file");
 
-fn main(){
-    let file = fs::read_to_string("tests/test_0.hs")
-        .expect("Error while reading the file");
-        
-        let parsed = HaskellParser::parse(Rule::program, file.as_str());
-        print_ast(parsed.unwrap(), 0);
+    let parsed = HaskellParser::parse(Rule::program, file.as_str()).unwrap();
+
+    println!("{:#?}", build_tree(parsed));
 }
 
-fn print_ast(ast : Pairs<'_, Rule>, offset: usize){
-use inline_colorization::*;
+pub fn print_ast(ast: Pairs<'_, Rule>, offset: usize) {
+    use inline_colorization::*;
     let spaces = if offset > 0 {
         format!("{}|   ", "    ".repeat(offset))
     } else {
         String::new()
     };
 
-    for pair in ast{
-        println!("{spaces}{color_green}{:?} {style_bold}{color_yellow}{:?}{style_reset}", pair.as_rule(), pair.as_str());
-        print_ast(pair.into_inner(), offset+1);
+    for pair in ast {
+        println!(
+            "{spaces}{color_green}{:?} {style_bold}{color_yellow}{:?}{style_reset}",
+            pair.as_rule(),
+            pair.as_str()
+        );
+        print_ast(pair.into_inner(), offset + 1);
     }
 }
 
 #[cfg(test)]
 mod tests {
-    
+
     #[test]
     fn single_file() {
         use super::*;
 
-        let file = fs::read_to_string("tests/test_0.hs")
-        .expect("Error while reading the file");
-        
+        let file = fs::read_to_string("tests/test_0.hs").expect("Error while reading the file");
+
         let parsed = HaskellParser::parse(Rule::program, file.as_str());
         if let Err(error) = &parsed {
             println!("{:#?}", error);
@@ -52,20 +58,17 @@ mod tests {
         use super::*;
 
         let test_dir = "tests/";
-        
-        let entries = fs::read_dir(test_dir)
-            .expect("Erro ao ler o diretório de testes");
+
+        let entries = fs::read_dir(test_dir).expect("Erro ao ler o diretório de testes");
 
         for entry in entries {
             let entry = entry.expect("Error while reading the folder");
             let path = entry.path();
-            
 
             if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("hs") {
                 let file_path = path.to_string_lossy();
 
-                let file = fs::read_to_string(&path)
-                    .expect("Error while reading the file");
+                let file = fs::read_to_string(&path).expect("Error while reading the file");
 
                 let parsed = HaskellParser::parse(Rule::program, file.as_str());
                 assert!(parsed.is_ok(), "Erro no parsing de '{}'", file_path);
