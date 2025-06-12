3386eaf64e2a3b5c4d3b3009747fdd7275d1e943
thales3006 <thales3006@gmail.com>
2025-06-11 23:29:28 -0300

    feat: test_0 with guards

--- tests/test_0.hs
+++ tests/test_0.hs
@@ -6,4 +6,13 @@ fac n = n * fac (n-1)
 -- add function, gets a number and add 1
 add :: Int -> Int
 add n = n+1
+
+-- list constant
 list = [3, add 3+1]
+
+-- fibonacci with guards
+fib :: Int -> Int
+fib n
+    | n<1 = 0
+    | n==1 = 1
+    | otherwise = fib(n-1) + fib(n-1)
